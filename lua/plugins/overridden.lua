4f46030272025e3ed81b6104d2a74b9ebce34028
GiperBoreipy <evgenchist207@gmail.com>
2025-05-09 16:19:18 +0300

    update

--- lua/plugins/overridden.lua
+++ lua/plugins/overridden.lua
@@ -16,4 +16,27 @@ return {{
       }
     }
   }
-}}
+}, 
+  {
+  "hrsh7th/nvim-cmp",
+  opts = function (_, opts)
+    cmp = require("cmp")
+    opts.sorting = {
+        priority_weight = 2,
+        comparators = {
+          cmp.config.compare.scopes,
+          cmp.config.compare.kind,
+          cmp.config.compare.sort_text,
+          cmp.config.compare.offset,
+          cmp.config.compare.exact,
+          cmp.config.compare.score,
+          cmp.config.compare.recently_used,
+          -- cmp.config.compare.length,
+          -- cmp.config.compare.order,
+        },
+      }
+    return opts
+  end
+}
+
+}
